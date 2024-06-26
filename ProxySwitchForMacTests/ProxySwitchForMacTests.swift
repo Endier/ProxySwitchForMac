990d7ad15893303f851e5d8fd1567ae427bcacb9
Cody <codycodey@icloud.com>
2024-06-26 09:01:29 +0800

    Initial Commit

---
+++ ProxySwitchForMacTests/ProxySwitchForMacTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProxySwitchForMacTests.swift
+//  ProxySwitchForMacTests
+//
+//  Created by Cody on 2024/6/26.
+//
+
+import Testing
+@testable import ProxySwitchForMac
+
+struct ProxySwitchForMacTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
