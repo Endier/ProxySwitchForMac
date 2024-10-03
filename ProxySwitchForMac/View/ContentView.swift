4b66f905c31c10b744460abd77aed394acf5d53c
Cody <codycodey@icloud.com>
2024-10-03 11:01:26 +0800

    Update ContentView.swift

--- ProxySwitchForMac/View/ContentView.swift
+++ ProxySwitchForMac/View/ContentView.swift
@@ -16,7 +16,6 @@ struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
 
     var body: some View {
-
         HStack {
             Form {
                 Section {
@@ -49,7 +48,7 @@ struct ContentView: View {
                     }
                     .onChange(of: appState.isOn) {
                         let serviceNames: [String] = ["Wi-Fi", "Ethernet"]
-                        let _ = setProxyEnable(
+                        _ = setProxyEnable(
                             serviceNames: serviceNames,
                             bool: appState.isOn)
                     }
@@ -58,19 +57,19 @@ struct ContentView: View {
                         "Customize global shortcut", name: .proxySwitch)
                 }
             }
-            .formStyle(.grouped)  // Form的这种格式类似于系统设置的选项卡风格
+            .formStyle(.grouped) // Form的这种格式类似于系统设置的选项卡风格
         }
     }
 }
 
-//Int：这意味着每个枚举成员都有一个与之关联的整数值，这个值默认从0开始。
-//Hashable：这意味着枚举类型可以被唯一地标识和比较。
-//CaseIterable：这意味着枚举类型可以提供一个包含所有成员的集合，可以用于迭代。
-//Identifiable：这意味着枚举类型有一个可以唯一标识其成员的属性id。
-//Codable：这意味着枚举类型可以被编码和解码，例如转换为JSON。
-//enum test: Int, Hashable, CaseIterable, Identifiable, Codable {
+// Int：这意味着每个枚举成员都有一个与之关联的整数值，这个值默认从0开始。
+// Hashable：这意味着枚举类型可以被唯一地标识和比较。
+// CaseIterable：这意味着枚举类型可以提供一个包含所有成员的集合，可以用于迭代。
+// Identifiable：这意味着枚举类型有一个可以唯一标识其成员的属性id。
+// Codable：这意味着枚举类型可以被编码和解码，例如转换为JSON。
+// enum test: Int, Hashable, CaseIterable, Identifiable, Codable {
 //
-//}
+// }
 
 func copyToClipboard(text: String) {
     let pasteBoard = NSPasteboard.general
